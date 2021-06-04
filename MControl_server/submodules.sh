b8035f2dbd7a6b3a9da87262cc68c0768dc70f5d
Vic Khov <victorKhovTko@yandex.ru>
2021-06-05 02:28:12 +0300

    Replaced lib:pycrypto with pycryptodome

--- MControl_server/submodules.sh
+++ MControl_server/submodules.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
 git submodule update --init --recursive
-pip install -U pycrypto
+# pip install -U pycrypto # <DEPRECATION>
+
+# info:
+#     PyCryptodome exposes almost the same API
+#     as the old PyCrypto so that most
+#     applications will run unmodified. 
+
+pip install -U pycryptodome
 pip install -U pysqlsimplecipher
